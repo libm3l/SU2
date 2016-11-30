311e9c1f4c2dbf108d47318a34206eb9a475353c
Adam Jirasek <jiraseka@shrike.rnet>
2016-11-30 11:42:53 -0700

    add prototype of communication function

---
+++ SU2_CFD/src/ext_comm.cpp
@@ -0,0 +1,66 @@
+/*!
+ * \file iteration_structure.cpp
+ * \brief Main subroutine managing communication to external solvers
+ * \author A. Jirasek
+ * \version 4.3.0 "Cardinal"
+ *
+ * SU2 Lead Developers: Dr. Francisco Palacios (Francisco.D.Palacios@boeing.com).
+ *                      Dr. Thomas D. Economon (economon@stanford.edu).
+ *
+ * SU2 Developers: Prof. Juan J. Alonso's group at Stanford University.
+ *                 Prof. Piero Colonna's group at Delft University of Technology.
+ *                 Prof. Nicolas R. Gauger's group at Kaiserslautern University of Technology.
+ *                 Prof. Alberto Guardone's group at Polytechnic University of Milan.
+ *                 Prof. Rafael Palacios' group at Imperial College London.
+ *                 Prof. Edwin van der Weide's group at the University of Twente.
+ *                 Prof. Vincent Terrapon's group at the University of Liege.
+ *
+ * Copyright (C) 2012-2016 SU2, the open-source CFD code.
+ *
+ * SU2 is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * SU2 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with SU2. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "../include/iteration_structure.hpp"
+
+#include "../include/ext_man_header.hpp"
+
+
+void External_Comm(CGeometry ***geometry_container, CSurfaceMovement **surface_movement,
+         CVolumetricMovement **grid_movement, CFreeFormDefBox ***FFDBox,CSolver ****solver_container,
+	 CConfig **config_container, unsigned short val_iZone, unsigned long IntIter,
+	 unsigned long ExtIter)   {
+
+  
+
+  unsigned short iDim, iMGlevel, nMGlevels = config_container[val_iZone]->GetnMGLevels();
+  unsigned short Kind_Grid_Movement = config_container[val_iZone]->GetKind_GridMovement(val_iZone);
+  unsigned long nIterMesh;
+  unsigned long iPoint;
+
+  d6dof_t d6dofdata, d6dofdata_old, *p_6DOFdata, *p_6DOFdata_old;
+  conn_t conn, *pconn;
+  struct timespec now, tmstart;
+  double seconds;
+
+  bool stat_mesh = true;
+  bool adjoint = config_container[val_iZone]->GetContinuous_Adjoint();
+  bool harmonic_balance = (config_container[val_iZone]->GetUnsteady_Simulation() == HARMONIC_BALANCE);
+
+  p_6DOFdata = &d6dofdata;
+  p_6DOFdata_old = &d6dofdata_old;
+  pconn = &conn;
+
+        cout << " Outside:   --------------------------  " << endl;
+ 
+}
---
+++ TestCases/unsteady/exteral_naca0012_euler/Client1_FakedSimulink.c
@@ -0,0 +1,238 @@
+/*
+ *     Copyright (C) 2015  Adam Jirasek
+ * 
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ * 
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ * 
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *     
+ *     contact: libm3l@gmail.com
+ * 
+ */
+
+
+
+/*
+ *     Function Client1.c
+ *
+ *     Date: 2015-02-23
+ * 
+ * 
+ *     Description:
+ * 
+ *
+ *     Input parameters:
+ * 
+ *
+ *     Return value:
+ * 
+ * 
+ *
+ *     Modifications:
+ *     Date		Version		Patch number		CLA 
+ *
+ *
+ *     Description
+ * 
+ */
+
+
+#include "libm3l.h"
+#include "lsipdx.h"
+
+int main(int argc, char *argv[])
+{
+	node_t *Gnode=NULL, *Snode=NULL, *FoundNode=NULL, *TmpNode=NULL;
+	size_t i, niter, dim[1], tot_dim;
+
+	lmint_t sockfd, portno;
+
+        socklen_t clilen;
+        struct sockaddr_in cli_addr;
+	lmchar_t *name ="CFD2SIM";
+	lmchar_t *name1="SIM2CFD";
+
+	lmdouble_t *P, dy, *tmpfloat, *x, *y, *z, *time, sign;
+	lmdouble_t psi, theta, phi;
+	
+	find_t *SFounds;
+	
+	opts_t opts, *Popts_1;
+	
+	FILE *fp;
+	
+	
+	client_fce_struct_t InpPar, *PInpPar;
+
+	PInpPar = &InpPar;
+/*
+ * get port number
+ */
+	if (argc < 3) {
+		fprintf(stderr,"ERROR, no IPaddress and port number provided\n");
+		exit(1);
+	}
+ 	portno = atoi(argv[2]);
+/*
+ * open socket - because we use more then just send - receive scenario
+ * we need to open socket manualy and used Send_receive function with hostname = NULL, ie. as server
+ * portno is then replaced by socket number
+ */
+	niter = 0;
+ 	while(1){
+
+ 		printf("\n\n--------------------------------    i = %ld\n\n", ++niter);
+/*
+ * open socket
+ */
+		PInpPar->channel_name = name;
+		PInpPar->SR_MODE = 'R';
+		if ( (PInpPar->mode = get_exchange_channel_mode('D', 'N')) == -1)
+			Error("wrong client mode");
+		Popts_1 = &opts;
+		m3l_set_Send_receive_tcpipsocket(&Popts_1);
+	
+		if( (sockfd = open_connection_to_server(argv[1], portno, PInpPar, Popts_1)) < 1)
+			Error("client_sender: Error when opening socket");
+		
+		
+		Gnode = client_receiver(sockfd, PInpPar, (opts_t *)NULL, (opts_t *)NULL);
+		
+		printf(" Data from SU2 received\n");
+/*
+ * locate ForcesMoments data
+ */
+		if( (SFounds = m3l_Locate(Gnode, "/CFD_2_SIM/ForcesMoments", "/*/*",  (lmchar_t *)NULL)) != NULL){
+
+			if( m3l_get_Found_number(SFounds) != 1)
+				Error("socket_SU2out: More then one DX data set found");
+/* 
+ * pointer to list of found nodes,
+ * theoretically /CFD_2_SIM/ can contain more then just one ForcesMoments so specify 
+ * so get a pointer on the first ForcesMoments node
+ */
+			if( (FoundNode = m3l_get_Found_node(SFounds, 0)) == NULL)
+				Error("socket_SU2out: Did not find 1st data pointer");
+			tot_dim = m3l_get_List_totdim(FoundNode);
+/*
+ * x is pointing on array containing values of force ForcesMoments
+ * size of array is tot_dim
+ * array is double (we know it, otherwise you can use a function to (lmchar_t *)m3l_get_List_type(FoundNode)
+ */		
+			if( (x = (lmdouble_t *)m3l_get_data_pointer(FoundNode)) == NULL)
+				Error("socket_SU2out: Did not find DX data pointer");
+/* 
+ * free memory allocated in m3l_Locate
+ */
+			m3l_DestroyFound(&SFounds);
+		}
+		else
+		{
+			Error("socket_SU2out: P not found\n");
+		}
+/*
+ * locate time
+ */	
+		SFounds = m3l_Locate(Gnode, "/CFD_2_SIM/Time", "/*/*",  (lmchar_t *)NULL);
+		FoundNode = m3l_get_Found_node(SFounds, 0);
+		time = (lmdouble_t *)m3l_get_data_pointer(FoundNode);
+		m3l_DestroyFound(&SFounds);
+
+		printf("Time is %lf  tot-dim is %ld\n", *time, tot_dim);
+/*
+ *  close socket
+ */
+		if( close(sockfd) == -1)
+			Perror("close");
+/*
+ *  sending data back
+ */
+
+/*
+ * make a list SIM_2_CFD
+ */
+		if(  (Snode = m3l_Mklist("SIM_2_CFD", "DIR", 0, 0, (node_t **)NULL, (const char *)NULL, (const char *)NULL, (char *)NULL)) == 0)
+			Perror("m3l_Mklist");
+
+		dim[0] = 3;
+/*
+ * add iteration number
+ */
+		if(  (TmpNode = m3l_Mklist("Angles", "D", 1, dim, &Snode, "/SIM_2_CFD", "./", (char *)NULL)) == 0)
+			Error("m3l_Mklist");
+		tmpfloat = (lmdouble_t *)m3l_get_data_pointer(TmpNode);
+/*
+ * 
+ *  psi - yaw
+ *  theta - pitch
+ *  phi - roll
+ */
+
+/*
+ * NACA 0012 CT1 case, or 2D cases the pithing is usually specified by rool angle
+ */
+		psi = -2.41*sin(*time*2*3.1415926*5.3398608);  
+		theta = 0.; 
+		phi = 0.;
+
+		printf("Pitch angles is %lf \nRoll angle is %lf \nYaw angle is %lf \n", theta, phi, psi);
+		
+		tmpfloat[0] = psi;
+		tmpfloat[1] = theta;
+		tmpfloat[2] = phi;
+/*
+ * add center of rotation
+ */
+		if(  (TmpNode = m3l_Mklist("RotCenter", "D", 1, dim, &Snode, "/SIM_2_CFD", "./", (char *)NULL)) == 0)
+			Error("m3l_Mklist");
+		tmpfloat = (lmdouble_t *)m3l_get_data_pointer(TmpNode);
+		tmpfloat[0] = 0.25;
+		tmpfloat[1] = 0.;
+		tmpfloat[2] = 0.;
+/*
+ * add vector of translation
+ */
+		if(  (TmpNode = m3l_Mklist("TransVec", "D", 1, dim, &Snode, "/SIM_2_CFD", "./", (char *)NULL)) == 0)
+			Error("m3l_Mklist");
+		tmpfloat = (lmdouble_t *)m3l_get_data_pointer(TmpNode);
+		tmpfloat[0] = 0;
+		tmpfloat[1] = 0;
+		tmpfloat[2] = 0;
+/*
+ * send the node
+ */
+		PInpPar->channel_name = name1;
+		PInpPar->SR_MODE = 'S';
+		if ( (PInpPar->mode = get_exchange_channel_mode('D', 'N')) == -1)
+			Error("wrong client mode");
+		Popts_1 = &opts;
+		m3l_set_Send_receive_tcpipsocket(&Popts_1);
+		
+		if( (sockfd = open_connection_to_server(argv[1], portno, PInpPar, Popts_1)) < 1)
+			Error("client_sender: Error when opening socket");
+		
+		client_sender(Snode, sockfd, PInpPar, (opts_t *)NULL, (opts_t *)NULL);
+
+		if(m3l_Umount(&Gnode) != 1)
+			Perror("m3l_Umount");
+		if(m3l_Umount(&Snode) != 1)
+			Perror("m3l_Umount");
+/* 
+ * close socket
+ */
+		if( close(sockfd) == -1)
+			Perror("close");
+
+ 	}
+
+
+     return 0; 
+}
